89f4c77feff7998ba6582abb035a9566ba05dd2e
lh03 <hexianjuan@gznb.com>
2019-03-08 13:04:39 +0800

    feat:add babel7 decorator demon at es6.vue

--- src/pages/es6.vue
+++ src/pages/es6.vue
@@ -9,97 +9,139 @@
 </template>
 
 <style lang="scss" scoped>
-    /*@import '@/assets/variable.scss';*/
-    
-    .bg {
-        background: $bg;
-        @include test(red);
-    }
+/*@import '@/assets/variable.scss';*/
+
+.bg {
+  background: $bg;
+  @include test(red);
+}
 </style>
 
 <script>
-    export default {
-        data() {
-            return {
-                isShow: false
-            }
-        },
-        methods: {
-            change() {
-                this.isShow = !this.isShow
-            },
-            way1() {
-                var students = {}
-                students[Symbol.iterator] = function() {
-                    let index = 1
-                    return {
-                        next() {
-                            return {
-                                done: index > 10,
-                                value: index++
-                            }
-                        }
-                    }
-                }
-
-                for (var i of students) {
-                    // i取得是value的值
-                    console.log(i)
-                }
-            },
-            way2() {
-                // 利用generator yield语法糖
-                console.log('-----我是利用generator yield语法糖实现for循环---------')
-                var students = {}
-                students[Symbol.iterator] = function*() {
-                    for (var i = 0; i < 11; i++) {
-                        yield i
-                    }
-                }
-                for (var i of students) {
-                    // i取得是value的值
-                    console.log(i)
-                }
-            },
-            way3() {
-                let students = {
-                        [Symbol.iterator]: function*() {
-                            for (var i = 0; i <= 10; i++) {
-                                yield i
-                            }
-                        }
-                    }
-                    // 所以generator返回的就是一个迭代器
-                var iterator = students[Symbol.iterator]()
-                var s = iterator.next()
-                while (!s.done) {
-                    console.log(s.value)
-                    s = iterator.next()
-                }
-            }
-        },
-        destroyed() {
-            alert('----destroyed----')
-        },
-        beforeCreate() {
-            alert('---beforeCreate----')
-        },
-        created() {
-            alert('----created----')
-        },
-        beforeMount() {
-            alert('----beforeMount')
-        },
-        beforeUpdate() {
-            alert('---beforeUpdate----')
-        },
-        updated() {
-            alert('----updated----')
-        },
-        mounted() {
-            alert('---mounted---')
-            this.way1()
-            this.way2()
-        }
+class C {
+  constructor(a, b) {
+    this.x = 12
+    this.init(a, b)
+  }
+  @enhanceInit()
+  init(a, b) {
+    debugger
+    this.a = a
+    this.b = b
+    console.log('--- start init----', this.a, this.b)
+  }
+  @enumerable(true)
+  method() { }
+}
+function enhanceInit(value) {
+  console.log('---enhanceInit---:', value)
+  return function (target, key, descriptor) {
+    const method = descriptor.value;
+    debugger
+    let moreDef = 100;
+    let ret;
+    descriptor.value = (...args) => {
+      args[0] += moreDef;
+      ret = method.apply(target, args);
+      return ret;
     }
+    return descriptor
+  }
+}
+function enumerable(value) {
+  return function (target, key, descriptor) {
+    //     debugger
+    //     console.log('----target, key, descriptor----', target, key, descriptor)
+    descriptor.enumerable = value;
+    return descriptor;
+  }
+}
+
+
+export default {
+  data() {
+    return {
+      isShow: false
+    }
+  },
+  methods: {
+    change() {
+      this.isShow = !this.isShow
+    },
+    way1() {
+      var students = {}
+      students[Symbol.iterator] = function () {
+        let index = 1
+        return {
+          next() {
+            return {
+              done: index > 10,
+              value: index++
+            }
+          }
+        }
+      }
+
+      for (var i of students) {
+        // i取得是value的值
+        console.log(i)
+      }
+    },
+    way2() {
+      // 利用generator yield语法糖
+      console.log('-----我是利用generator yield语法糖实现for循环---------')
+      var students = {}
+      students[Symbol.iterator] = function* () {
+        for (var i = 0; i < 11; i++) {
+          yield i
+        }
+      }
+      for (var i of students) {
+        // i取得是value的值
+        console.log(i)
+      }
+    },
+    way3() {
+      let students = {
+        [Symbol.iterator]: function* () {
+          for (var i = 0; i <= 10; i++) {
+            yield i
+          }
+        }
+      }
+      // 所以generator返回的就是一个迭代器
+      var iterator = students[Symbol.iterator]()
+      var s = iterator.next()
+      while (!s.done) {
+        console.log(s.value)
+        s = iterator.next()
+      }
+    }
+  },
+  destroyed() {
+    //     alert('----destroyed----')
+  },
+  beforeCreate() {
+    //     alert('---beforeCreate----')
+  },
+  created() {
+    //     alert('----created----')
+  },
+  beforeMount() {
+    //     alert('----beforeMount')
+  },
+  beforeUpdate() {
+    //     alert('---beforeUpdate----')
+  },
+  updated() {
+    //     alert('----updated----')
+  },
+  mounted() {
+    //     alert('---mounted---')
+    let c = new C(1, 2)
+    console.log(Object.keys(C.prototype))
+    this.way1()
+    this.way2()
+  }
+}
 </script>
